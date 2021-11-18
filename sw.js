a0874302196d4091642bafeb1575fb3566c53453
Marco Antonio Ruiz Rojas <ocram_cia@hotmail.com>
2021-11-18 14:49:15 -0600

    Modificamos el service worker

--- sw.js
+++ sw.js
@@ -8,24 +8,24 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 
 
 const APP_SHELL = [
-    '/',
+    //'/',
     'index.html',
-    '/css/style.css',
-    '/img/favicon.ico',
-    '/js/app.js',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg'
+    'css/style.css',
+    'img/favicon.ico',
+    'js/app.js',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg'
 ];
 
 const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    '/css/animate.css',
-    '/js/libs/jquery.js'
+    'css/animate.css',
+    'js/libs/jquery.js'
 ];
 
 self.addEventListener( 'install', e => {
